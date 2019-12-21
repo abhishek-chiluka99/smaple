837fb77b2882b3c8868dcd340fdda7fdb3dfda62
abhishek-chiluka99 <chilukaabhishek9@gmail.com>
2019-12-21 17:13:19 +0530

    agaiin

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ console.log("Hello World");
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-app.use(express.static('smaple'))
-
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
