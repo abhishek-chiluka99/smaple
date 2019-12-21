9f10f5016b0870d89ec016597669ff78a19e865b
abhishek-chiluka99 <chilukaabhishek9@gmail.com>
2019-12-21 18:01:19 +0530

    html added

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-
 const express = require('express')
 const app = express()
 const port =process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendfile(__dirname+'/hello.html'))
+app.use(express.static('smaple'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log('Example app listening on port ${port}!'))
