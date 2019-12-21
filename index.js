e7cce3fb8d24b0b8b0a5e26d9fa778348e30a767
abhishek-chiluka99 <chilukaabhishek9@gmail.com>
2019-12-21 20:41:17 +0530

    new index

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.sendfile(__dirname+'/public/html/hello.html'))
+app.get('/', (req, res) => res.sendfile(__dirname+'/public/html/dest.html'))
 app.use(express.static('public'))
 
 app.listen(port, () => console.log('Example app listening on port ${port}!'))
