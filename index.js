318aebc2cc6334f5a5a496aa8a8051a4996b4727
abhishek-chiluka99 <chilukaabhishek9@gmail.com>
2019-12-22 13:21:00 +0530

    new html

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/htmls/index.html'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/index.html'))
 
 var obj={}
 obj.name = "Being Zero";
@@ -12,4 +12,4 @@ obj.regno = "111111ABC";
 
 app.get('/data', (req, res) => res.json(obj))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log('Example app listening on port ${port}!'))
