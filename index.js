0f3cabe800da41af751e9efcead95bb8eb12cd2b
abhishek-chiluka99 <chilukaabhishek9@gmail.com>
2019-12-22 13:23:17 +0530

    new html

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/index.html'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/hello.html'))
 
 var obj={}
 obj.name = "Being Zero";
